a23e5e585d87ebdb41b9e69116ed8d1ac7766a57
Luca Varini <luca.vr93@gmail.com>
2022-02-11 17:26:03 +0100

    initial commit for reducing bank of words

--- semirandom_player.py
+++ semirandom_player.py
@@ -3,12 +3,16 @@ import random
 import time
 
 
-class RandomPlayer(Player):
+class SemiRandomPlayer(Player):
     def __init__(self, name, word_bank):
         self.name = name
         self.word_bank = word_bank
+        self.knowledge = {} #dictionary with the same format of the result plus the record 'letter not in word'
+        self.letter_not_in_word = {[]} # format: list of    'letter': 'A'
+                                       #                    'position': [1, 2, 3]
     
-    def choose(self, alphabet, word_bank, i):
+    def choose(self, i, letter_not_in_word):
+        self.word_bank = self.reduceWordBank(self.word_bank, letter_not_in_word)
         attempt = random.choice(self.word_bank)
         print(f"Attempt #{i+1}: {attempt}")
         time.sleep(3)
@@ -16,3 +20,13 @@ class RandomPlayer(Player):
 
     def __str__(self):
         return self.name
+    
+    #this function iterate on each letter of the knowledge so far and select 
+    # all the words that match with the letter considered
+    # For example: knowledge = 'A _ _ I O' 
+    def reduceWordBank(self, word_bank, letter_not_in_word):
+        reduced_word_bank = []
+        for word in word_bank:
+            for pos, char in word: #check the green ones first (with an eye on letters not in word)
+                continue
+        return reduced_word_bank
