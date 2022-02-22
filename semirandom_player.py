d76207d010f620335405ef91947c320341002eae
Luca Varini <luca.vr93@gmail.com>
2022-02-22 10:51:08 +0100

    added yellow logic filtering

--- semirandom_player.py
+++ semirandom_player.py
@@ -40,24 +40,31 @@ class SemiRandomPlayer(Player):
             print(f'Word bank reduced of {len(word_bank)-len(newWordBank)} words by elimination')
         return newWordBank
     
-    def checkLettersInWord(self, word_bank, letter_in_word):
+    def checkLettersInWord(self, word_bank, letter_in_word): #only purpose, for now, is to filter words that contains yellow letters
         newWordBank = word_bank
         if len(letter_in_word.keys())!=0:
             result = []
+            yellow_letters = []
             for letter in letter_in_word.keys():
-                positions = letter_in_word[letter]
-                for pos in positions:
-                    temp = [word for word in word_bank if word[pos] == letter]
+                '''
+                if len(letter_in_word[letter]) == 0: # so if it's a yellow letter with position unknown
+                    temp = [word for word in word_bank if letter in word]
                     result.extend([word for word in temp if word not in result]) # copy in result without duplicates
-            newWordBank = [word for word in word_bank if word in result]
-            print(f'Word bank reduced of {len(word_bank)-len(newWordBank)} words by keeping only words with green letters right')
+                '''
+                if len(letter_in_word[letter]) == 0: # so if it's a yellow letter with position unknown
+                    yellow_letters.append(letter)
+            if len(yellow_letters) != 0:
+                temp = [word for word in word_bank if all(x in word for x in yellow_letters)] #extracts words that contains ALL yellow letters at once. Not one or another
+                result.extend([word for word in temp if word not in result])
+                newWordBank = [word for word in word_bank if word in result]
+            print(f'Word bank reduced of {len(word_bank)-len(newWordBank)} words by keeping only words with yellow letters')
         return newWordBank
     
     #this function iterate on each letter of the knowledge so far and select 
     # all the words that match with the letter considered
     def reduceWordBank(self, word_bank, letter_not_in_word, letter_in_word):
         reduced_word_bank = self.checkLettersNotInWord(word_bank, letter_not_in_word)
-        #reduced_word_bank = self.checkLettersInWord(reduced_word_bank, letter_in_word)
+        reduced_word_bank = self.checkLettersInWord(reduced_word_bank, letter_in_word)
 
         return reduced_word_bank
     
