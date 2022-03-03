2271363eb23127dc2611fddd663b7e6232ecfda2
Luca Varini <luca.vr93@gmail.com>
2022-03-03 20:48:11 +0100

    number of matches is now a parameter in settings

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from intelligent_player import IntelligentPlayer
 from wordle import gameHandler
 from human_player import HumanPlayer
 from random_player import RandomPlayer
-from settings import FILENAME
+from settings import FILENAME, NUMBER_OF_MATCHES
 
 #test
 
 attempt_list = []
 
 #while True:
-for i in range(100):
+for i in range(NUMBER_OF_MATCHES):
     fileName = FILENAME
     f = open(fileName)
     parole = f.read()
@@ -27,4 +27,4 @@ for i in range(100):
         print('\nThanks for playing.\n')
         exit()
     '''
-    print(f'The run has concluded with a score of {sum(attempt_list)/len(attempt_list)}')
+print(f'The run has concluded with a score of {sum(attempt_list)/len(attempt_list)}')
